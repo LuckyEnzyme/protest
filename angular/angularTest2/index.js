723524c1e5386aa81f81f7088a90c3e32665fb37
gaoyamei <gaoyamei@sendcloud.im>
2018-10-10 18:16:01 +0800

    2018/10/10

--- angular/angularTest2/index.js
+++ angular/angularTest2/index.js
@@ -26,7 +26,8 @@ myApp.config(["$stateProvider","$urlRouterProvider", function($stateProvider,$ur
             templateUrl:"Page5.html"
         });
 }]);
-myApp.controller('myCtrl', ['$scope', function ($scope) {
+myApp.controller('myCtrl', ['$scope', 'config', function ($scope, config) {
+    console.log(config,'常量')
     $scope.nav=[
         {id:1,name:'首页'},{id:2,name:'日志'},{id:3,name:'策略'},{id:4,name:'设置'},{id:5,name:'关于'}
     ];
@@ -34,6 +35,7 @@ myApp.controller('myCtrl', ['$scope', function ($scope) {
         id:1,
         name:"首页"
     };
+    $scope.olnavText = '';
     $scope.nav.filter(it=>{
         if(it.id==1){
             it.status=true;
@@ -56,26 +58,77 @@ myApp.controller('myCtrl', ['$scope', function ($scope) {
         });
         //console.log($scope.nav);
     };
+    $scope.$watch("olnavText",function(n,o){
+        //console.log(o);
+        if(n!=''){
+            $scope.$broadcast("olNavChange", n);
+        }
+        //$scope.$broadcast("olNavChange", n);
+    });
+
+    $scope.$on("child1Change",function(e,m){
+        $scope.olnavText=m;
+        $scope.$broadcast("child2Change",m)
+    });
 }]);
-myApp.controller('Page1Controller', ['$scope', '$http',function ($scope,$http) {
+myApp.controller('Page1Controller', ['$scope', '$http','Data',function ($scope,$http,Data) {
+    $scope.searchTxt = '';
+    //$scope.child = '';
     $http({
         method:'GET',
         url:'http://127.0.0.1:3000/data/testlist/list'
     }).then(function(res){
         $scope.list=res.data.data;
-        console.log($scope.list);
+        //console.log($scope.list);
+    });
+    //父子控制器的作用域继承，子控制器可以访问父控制器上的数据
+    $scope.parentNav=$scope.$parent.nav;
+    $scope.data=Data;
+   
+    //上级向下级传递消息
+    $scope.$on("olNavChange",function(e,m){
+        //console.log(m);
+        $scope.child=m;
+    });
+
+    //下级向上级传递消息
+    $scope.$watch("child",function(n,o){
+        $scope.$emit("child1Change",n);
     });
 }]);
-myApp.controller('Page2Controller', ['$scope','$http',function ($scope,$http) {
+myApp.controller('Page2Controller', ['$scope','$http','Data',function ($scope,$http,Data) {
+    $scope.child = '';
     $http({
         method:'GET',
         url:'http://127.0.0.1:3000/data/testlist/list'
     }).then(function(res){
         $scope.list2=res.data.data;
     });
+    $scope.parentNav=$scope.$parent.nav;
+    $scope.data=Data;
+
+    $scope.$on("olNavChange",function(e,m){
+        console.log(m);
+        $scope.child=m;
+    });
+
+    $scope.$on("child2Change",function(e,m){
+        $scope.child=m;
+    });
+
 }]);
 myApp.filter('changeStatus',function(){
     return function(status){
         return status?'正常':'异常';
     }
+});
+//定义一个service，可以用于任何需要通信的controller之间
+myApp.service('Data', [function () {
+    var data={};
+    return data;
+}]);
+
+myApp.constant('config', {
+    'name' : 'sendcloud',
+    'key': 'sd'
 })
